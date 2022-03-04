40ab697028650a2f027a8afa4ffe0ce9116c10e2
Caio Celso <bliteti.caio@unidas.com.br>
2022-03-03 21:03:46 -0300

    removendo lazy-loading

--- front-end/victor-santini-front/src/app/app-routing.module.ts
+++ front-end/victor-santini-front/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [{
-  path:'',
-  children: [
-    {path: 'home', data:{}, loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)}
-]
-}];
+const routes: Routes = [
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
